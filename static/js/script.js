76619bbb36ab2b9608c5668b947555092c4e81e3
antoniolac <131681953+antoniolac@users.noreply.github.com>
2024-10-01 21:30:51 +0200

    function e impl.game

---
+++ static/js/script.js
@@ -0,0 +1,39 @@
+//funzione che genere un numero casuale
+function generateRandomInteger(min = 1, max = 100){
+    return Math.floor(Math.random() * (max - min + 1) + min) ;
+}
+
+//funzione per il sanitize dell'input
+function sanitize(stringa) {
+    return parseInt(stringa.trim());
+}
+
+//variabile che memorizza il numero random
+let nRandom = generateRandomInteger();
+console.log(nRandom);
+
+//variabile che tiene conto dei tentativi
+let i = 0; 
+
+//variabile di input
+let guess;
+while (i < 5 && guess !== nRandom ) {
+
+    do {
+        guess = prompt("indovina il numero")
+    } while (guess === null || guess.trim() === "" || isNaN(guess));
+    guess = sanitize(guess);
+    console.log(guess);
+    if (guess > nRandom)
+        console.log("Troppo grande");
+    else if (guess < nRandom)
+        console.log("Troppo piccolo");
+    i++;
+}
+
+if (guess === nRandom) 
+    console.log("Bravo, hai vinto");
+else if (i === 5)
+    console.log("Hai perso, il numero era " + nRandom);
+
+
