971e9082488ee3e6061b4fca122f014b35b5885c
antoniolac <131681953+antoniolac@users.noreply.github.com>
2024-10-08 09:46:01 +0200

    add modellazione dom

--- static/js/script.js
+++ static/js/script.js
@@ -1,40 +1,41 @@
-//funzione che genere un numero casuale
-function generateRandomInteger(min = 1, max = 100){
-    return Math.floor(Math.random() * (max - min + 1) + min) ;
-}
 
-//funzione per il sanitize dell'input
-function sanitize(stringa) {
+// Funzione che genera un numero casuale
+function generateRandomInteger(min = 1, max = 100) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
+  // Funzione per il sanitize dell'input
+  function sanitize(stringa) {
     return parseInt(stringa.trim());
-}
-
-//variabile che memorizza il numero random
-let nRandom = generateRandomInteger();
-console.log(nRandom);
-
-//variabile che tiene conto dei tentativi
-let i = 0; 
-
-//variabile di input
-let guess;
-console.log("indovina il numero, hai 5 tentativi");
-while (i < 5 && guess !== nRandom ) {
-
-    do {
-        guess = prompt("indovina il numero")
-    } while (guess === null || guess.trim() === "" || isNaN(guess));
-    guess = sanitize(guess);
+  }
+  
+  function handleClick(event) {
+    guess = sanitize(document.querySelector("#guess").value); // Get the input value and sanitize it
     console.log(guess);
-    if (guess > nRandom)
-        console.log("Troppo grande");
-    else if (guess < nRandom)
-        console.log("Troppo piccolo");
-    i++;
-}
-
-if (guess === nRandom) 
-    console.log("Bravo, hai vinto");
-else if (i === 5)
-    console.log("Hai perso, il numero era " + nRandom);
+    if (guess > nRandom) {
+      console.log("Troppo grande");
+      i++;
+    } else if (guess < nRandom) {
+      console.log("Troppo piccolo");
+      i++;
+    } else { // Winning condition
+      console.log("Bravo, hai vinto");
+      
+    }
+    if (i === 5) {
+      console.log("Hai perso, il numero era " + nRandom);
+    }
+  }
+  
+  // Variabile che memorizza il numero random
+  let nRandom = generateRandomInteger();
+  console.log(nRandom);
+  
+  // Variabile che tiene conto dei tentativi
+  let i = 0;
+  
+  let btn = document.querySelector("#guessBtn");
+  // Evento al click del bottone
+  btn.addEventListener("click", handleClick);
 
 
