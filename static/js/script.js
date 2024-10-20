a8cc2f7b5c6c2f3b191207132a4ad74c5f6ac75d
antoniolac <131681953+antoniolac@users.noreply.github.com>
2024-10-20 16:15:23 +0200

    add innerHTML

--- static/js/script.js
+++ static/js/script.js
@@ -10,20 +10,27 @@ function generateRandomInteger(min = 1, max = 100) {
   }
   
   function handleClick(event) {
-    guess = sanitize(document.querySelector("#guess").value); // Get the input value and sanitize it
+    guess = sanitize(document.querySelector("#guess").value); 
     console.log(guess);
+    
+    let help;
+    let tentativi;
     if (guess > nRandom) {
-      console.log("Troppo grande");
+      help = document.querySelector("#help").innerHTML = "Troppo grande"
       i++;
+      tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
     } else if (guess < nRandom) {
-      console.log("Troppo piccolo");
+      help = document.querySelector("#help").innerHTML = "Troppo piccolo"
       i++;
-    } else { // Winning condition
-      console.log("Bravo, hai vinto");
-      
+      tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
+    } else { // Win condition
+      help = document.querySelector("#help").innerHTML = "Bravo hai vinto!"
+      i++;
+      tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
+      let win = document.querySelector("#win").innerHTML = "hai impiegato " + i + " tentativi"      
     }
     if (i === 5) {
-      console.log("Hai perso, il numero era " + nRandom);
+      help = document.querySelector("#help").innerHTML = "Hai perso, il numero era " + nRandom
     }
   }
   
@@ -33,6 +40,7 @@ function generateRandomInteger(min = 1, max = 100) {
   
   // Variabile che tiene conto dei tentativi
   let i = 0;
+  tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
   
   let btn = document.querySelector("#guessBtn");
   // Evento al click del bottone
