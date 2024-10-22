68aa7245a9a91b9414cf432d51006565df90b25c
antoniolac <131681953+antoniolac@users.noreply.github.com>
2024-10-22 17:53:10 +0200

    fix js code

--- static/js/script.js
+++ static/js/script.js
@@ -1,77 +1,104 @@
+/*
+    GLOBAL
+            */
+
+//numero dei tentativi disponibile
+const TENT = 5;
 // Variabile che memorizza il numero random
 let nRandom = generateRandomInteger();
 console.log(nRandom);
   
-// Variabile che tiene conto dei tentativi
+// Variabile contatore dei tentativi
 let i = 0;
-tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
-  
+let tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (TENT-i) + " tentativi";
+
+//bottone
 let btn = document.querySelector("#guessBtn");
 // Evento al click del bottone
 btn.addEventListener("click", handleClick);
 
 
+/* 
+  FUNZIONI E PROCEDURE
+                      */
 
 // Funzione che genera un numero casuale
 function generateRandomInteger(min = 1, max = 100) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   
-  // Funzione per il sanitize dell'input
-  function sanitize(stringa) {
-    return parseInt(stringa.trim());
+// Funzione per il sanitize dell'input
+function sanitize(stringa) {
+  return parseInt(stringa.trim());
+}
+
+
+//funzione che gestisce l'evento click del bottone
+function handleClick(event) {
+  //sanitize input
+  let guess = sanitize(document.querySelector("#guess").value); 
+  console.log(guess);
+
+  //gestione dei tentativi e controllo del valore di input rispetto al numero random
+  let help; //variabile per gli indizi
+  let tentativi;// variabile per i tentativi rimanenti
+
+  //background-color in base al numero dei tentativi
+  let element = document.querySelector("#Bg");
+
+  let win = document.querySelector("#win")//messaggio di output in un h5
+  //controlli
+  
+  if (guess === nRandom)  { // Win condition
+    help = document.querySelector("#win").innerHTML = "Bravo hai vinto!"
+    i++
+    tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (TENT-i) + " tentativi";
+    win = document.querySelector("#help").innerHTML = "hai impiegato " + i + " tentativi"
+
+    //se si vince, dopo 5sec si aggiorna la pagina per rigiocare
+    setTimeout(() => {
+      location.reload();
+    }, 5000);
+    //disabilitazione del bottone
+    btn.disabled = true;
+    return;    
+  }
+  else if (guess > nRandom) { 
+    help = document.querySelector("#help").innerHTML = "Troppo grande"
+    
+  }
+  else if (guess < nRandom) {
+    help = document.querySelector("#help").innerHTML = "Troppo piccolo"
   }
   
-  function handleClick(event) {
-    guess = sanitize(document.querySelector("#guess").value); 
-    console.log(guess);
+  //tentativi rimanenti
+  i++
+  tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (TENT-i) + " tentativi";
+ 
+  //background-color in base al numero dei tentativi
+  if(i === 2){
+    element.classList.remove("hero-green");
+    element.classList.add("hero-yellow");
 
-
-    let element = document.querySelector("#Bg");
-    if(i === 1){
-      element.classList.remove("hero-green");
-      element.classList.add("hero-yellow");
-
-      btn.classList.remove("b-green");
-      btn.classList.add("b-yellow");
-    }
-    else if (i == 4){
-      element.classList.remove("hero-yellow");
-      element.classList.add("hero-red");
-
-      btn.classList.remove("b-yellow");
-      btn.classList.add("b-red");
-    }
-
-
-    let help;
-    let tentativi;
-    if (guess > nRandom) {
-      help = document.querySelector("#help").innerHTML = "Troppo grande"
-      i++;
-      tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
-    } else if (guess < nRandom) {
-      help = document.querySelector("#help").innerHTML = "Troppo piccolo"
-      i++;
-      tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
-    } else { // Win condition
-      help = document.querySelector("#help").innerHTML = "Bravo hai vinto!"
-      i++;
-      //tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
-      let win = document.querySelector("#win").innerHTML = "hai impiegato " + i + " tentativi"
-      setTimeout(() => {
-        location.reload();
-      }, 5000);
-      btn.disabled = true     
-    }
-    if (i === 5) {
-      help = document.querySelector("#help").innerHTML = "Hai perso, il numero era " + nRandom
-      setTimeout(() => {
-        location.reload();
-      }, 5000); 
-      btn.disabled = true
-    }
+    btn.classList.remove("b-green");
+    btn.classList.add("b-yellow");
   }
+  else if (i === TENT){
+    element.classList.remove("hero-yellow");
+    element.classList.add("hero-red");
+
+    btn.classList.remove("b-yellow");
+    btn.classList.add("b-red");
+
+    help = document.querySelector("#help").innerHTML = "Hai perso, il numero era " + nRandom
+    //se si perde, dopo 5sec si aggiorna la pagina per rigiocare
+    setTimeout(() => {
+      location.reload();
+    }, 5000); 
+    //disabilitazione del bottone
+    btn.disabled = true
+  }
+}
 
 
 
