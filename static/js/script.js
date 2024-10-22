7b172663b66da9756398de858a0cc936e59b202c
antoniolac <131681953+antoniolac@users.noreply.github.com>
2024-10-22 09:47:05 +0200

    include classList

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,16 @@
+// Variabile che memorizza il numero random
+let nRandom = generateRandomInteger();
+console.log(nRandom);
+  
+// Variabile che tiene conto dei tentativi
+let i = 0;
+tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
+  
+let btn = document.querySelector("#guessBtn");
+// Evento al click del bottone
+btn.addEventListener("click", handleClick);
+
+
 
 // Funzione che genera un numero casuale
 function generateRandomInteger(min = 1, max = 100) {
@@ -12,7 +25,25 @@ function generateRandomInteger(min = 1, max = 100) {
   function handleClick(event) {
     guess = sanitize(document.querySelector("#guess").value); 
     console.log(guess);
-    
+
+
+    let element = document.querySelector("#Bg");
+    if(i === 1){
+      element.classList.remove("hero-green");
+      element.classList.add("hero-yellow");
+
+      btn.classList.remove("b-green");
+      btn.classList.add("b-yellow");
+    }
+    else if (i == 4){
+      element.classList.remove("hero-yellow");
+      element.classList.add("hero-red");
+
+      btn.classList.remove("b-yellow");
+      btn.classList.add("b-red");
+    }
+
+
     let help;
     let tentativi;
     if (guess > nRandom) {
@@ -26,24 +57,25 @@ function generateRandomInteger(min = 1, max = 100) {
     } else { // Win condition
       help = document.querySelector("#help").innerHTML = "Bravo hai vinto!"
       i++;
-      tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
-      let win = document.querySelector("#win").innerHTML = "hai impiegato " + i + " tentativi"      
+      //tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
+      let win = document.querySelector("#win").innerHTML = "hai impiegato " + i + " tentativi"
+      setTimeout(() => {
+        location.reload();
+      }, 5000);
+      btn.disabled = true     
     }
     if (i === 5) {
       help = document.querySelector("#help").innerHTML = "Hai perso, il numero era " + nRandom
+      setTimeout(() => {
+        location.reload();
+      }, 5000); 
+      btn.disabled = true
     }
   }
-  
-  // Variabile che memorizza il numero random
-  let nRandom = generateRandomInteger();
-  console.log(nRandom);
-  
-  // Variabile che tiene conto dei tentativi
-  let i = 0;
-  tentativi = document.querySelector("#tentativi").innerHTML = "Hai ancora " + (5-i) + " tentativi";
-  
-  let btn = document.querySelector("#guessBtn");
-  // Evento al click del bottone
-  btn.addEventListener("click", handleClick);
+
+
+
+
+
 
 
